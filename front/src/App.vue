6f58b7ee81d5c5aa47f763ae00677af1bba736a8
thewalkers <1518832673@qq.com>
2021-09-13 11:32:34 +0800

    start: 开始前端开发

---
+++ front/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>这里是 App 根组件</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
---
+++ front/src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './assets/bootstrap.min.css'
+import './index.css'
+
+createApp(App).mount('#app')
