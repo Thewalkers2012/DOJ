3bb64d456094b90b62b57cd48fcc093521c1e156
thewalkers <1518832673@qq.com>
2021-09-14 20:01:25 +0800

    finish：初始化 axios

--- front/src/main.js
+++ front/src/main.js
@@ -3,10 +3,13 @@ import App from './App.vue'
 import './assets/bootstrap.min.css'
 import './assets/bootstrap.min.js'
 import router from './router/index'
-import './index.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 const app = createApp(App)
-
+// vue router
 app.use(router)
-
+// axios 
+app.use(VueAxios, axios)
+// 挂载 app 节点
 app.mount('#app')
