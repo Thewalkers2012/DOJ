9591db53022c5f9a515424738b6ed6774cf1134a
thewalkers <1518832673@qq.com>
2021-09-17 20:10:33 +0800

    finish: vscode 代码编辑框

---
+++ front/src/components/edit/Edit.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="container" style="width: 1000px; height: 600px"></div>
+  <!--宽高自行设定 -->
+</template>
+
+<script>
+import * as monaco from 'monaco-editor';
+
+export default {
+  data() {
+    return {
+      editor: null,
+    };
+  },
+  mounted() {
+    this.initEditor();
+  },
+  methods: {
+    initEditor() {
+      this.editor = monaco.editor.create(document.getElementById('container'), {
+        language: 'java',
+        value: '',
+        automaticLayout: true,
+        theme: 'vs-light',
+        selectOnLineNumbers: true,
+        roundedSelection: false,
+        readOnly: false,
+        cursorStyle: 'line',
+        glyphMargin: true,
+        useTabStops: false,
+        fontSize: 16,
+        autoIndent: true,
+        quickSuggestionsDelay: 500,
+      });
+    },
+    getValue() {
+      this.editor.getValue();
+    },
+  },
+};
+</script>
--- front/src/components/problem.vue
+++ front/src/components/problem.vue
@@ -35,7 +35,8 @@
       </div>
       <!-- 代码编辑区域 -->
       <b-card class="mt-5" title="编辑器">
-        <pre v-highlightjs><code class="cpp">{{ code }}</code></pre>
+        <Edit> </Edit>
+        <!-- <pre v-highlightjs><code class="cpp">{{ code }}</code></pre> -->
       </b-card>
     </b-card>
   </div>
@@ -43,71 +44,21 @@
 
 <script>
 import problemService from '../service/problemService';
+import Edit from './edit/Edit.vue';
 
 export default {
   data() {
     return {
       problem: {},
-      code: `
-#include <bits/stdc++.h>
+      code: `#include <bits/stdc++.h>
 using namespace std;
 
-const int MAX_N = 100010;
-
-int h[MAX_N], e[MAX_N], w[MAX_N], ne[MAX_N], idx;
-
-void add(int a, int b, int c) {
-  e[idx] = b, w[idx] = c, ne[idx] = h[a], h[a] = idx++;
-}
-
-int n, m, s;
-vector<pair<int, int>> adj[MAX_N];
-long long dist[MAX_N];
-
-typedef pair<int, int> pii;
-
-void dijkstra() {
-  priority_queue<pii, vector<pii>, greater<pii>> heap;
-  vector<bool> vis(n);
-  heap.push(make_pair(0, s));
-  memset(dist, 0x3f, sizeof dist);
-  dist[s] = 0;
-  while (!heap.empty()) {
-    auto t = heap.top();
-    heap.pop();
-    int ver = t.second;
-    if (vis[ver]) {
-      continue;
-    }
-    vis[ver] = true;
-    for (auto &v : adj[ver]) {
-      if (dist[v.first] > dist[ver] + v.second) {
-        dist[v.first] = dist[ver] + v.second;
-        heap.push({dist[v.first], v.first});
-      }
-    }
-  }
-  for (int i = 1; i <= n; i++) {
-    cout << dist[i] << ' ';
-  }
-}
-
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(0);
-
-  cin >> n >> m >> s;
-  while (m--) {
-    int a, b, c;
-    cin >> a >> b >> c;
-    adj[a].push_back(make_pair(b, c));
-  }
-
-  dijkstra();
-
+  int a, b;
+  cin >> a >> b;
+  cout << a + b << endl;
   return 0;
-}
-      `,
+}`,
     };
   },
   created() {
@@ -119,6 +70,9 @@ int main() {
       const { data: res } = await problemService.getProblemByID(id);
       this.problem = res.data.problem;
     },
+  },
+  components: {
+    Edit,
   },
 };
 </script>
