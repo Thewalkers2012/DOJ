b8ce02fc076937ed663655cea1bb05e3a2fa8251
thewalkers <1518832673@qq.com>
2021-09-29 16:34:34 +0800

    test python1

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 
 # Author: litianyu
 # Description: 通过 Python 基于 bs4 的网络爬虫。
+# How to use it: 需要修改 headers 中的内容，可以从各自浏览器中输入 about://version 来进行查看 `User-Agent` 内容
 
+from os import write
 import urllib.request
 from bs4 import BeautifulSoup
+from tenacity import retry
 import json
 
+BaseUrl = 'https://theme.npm.edu.tw/opendata/DigitImageSets.aspx'
 
+
+@retry
 def handle_request(url):
     headers = {
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) \
@@ -16,11 +22,14 @@ def handle_request(url):
     return request
 
 
+# @retry
 def wirte_to_file(url):
     request = handle_request(url)
+    # print(url)
 
     # 发送请求等待相应
-    content = urllib.request.urlopen(request).read().decode('utf-8')
+    content = urllib.request.urlopen(
+        request, timeout=10).read().decode('utf-8')
     # 解析内容
     soup = BeautifulSoup(content, 'lxml')
     # 获取对应的部分
@@ -30,12 +39,9 @@ def wirte_to_file(url):
 
     fileName = ul[0].text[7:] + '.json'
 
-    map[ul[0].text[:6]] = ul[0].text[7:]
-    map[ul[1].text[:2]] = ul[1].text[3:]
-    map[ul[2].text[:2]] = ul[2].text[3:]
-    map[ul[3].text[:2]] = ul[3].text[3:]
-    map[ul[4].text[:2]] = ul[4].text[3:]
-    map[ul[5].text[:3]] = ul[5].text[4:]
+    for li in ul:
+        map[li.text[:li.text.index('：')]
+            ] = li.text[li.text.index('：') + 1:]
 
     # dict 转化为 json
     j = json.dumps(map, ensure_ascii=False)
@@ -46,22 +52,24 @@ def wirte_to_file(url):
     file.close()
 
 
+@retry
 def get_link(baseUrl):
     request = handle_request(baseUrl)
     # 发送请求等待相应
-    content = urllib.request.urlopen(request).read().decode('utf-8')
+    content = urllib.request.urlopen(
+        request, timeout=10).read().decode('utf-8')
     # 解析内容
     soup = BeautifulSoup(content, 'lxml')
     # 获取对应的部分
     ul = soup.select('.painting-list > li > a')
     for li in ul:
-        childUrl = baseUrl + li['href'][19:]
+        childUrl = BaseUrl + li['href'][19:]
         wirte_to_file(childUrl)
 
 
 # last page url is 2331
 if __name__ == '__main__':
-    baseUrl = 'https://theme.npm.edu.tw/opendata/DigitImageSets.aspx'
-    for i in range(1, 5):
+    for i in range(93, 500):
         # print(baseUrl + '?pageNo=' + str(i))
-        get_link(baseUrl + '?pageNo=' + str(i))
+        print(BaseUrl + '?pageNo=' + str(i))
+        get_link(BaseUrl + '?pageNo=' + str(i))
