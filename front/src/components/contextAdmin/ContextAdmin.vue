2a2eb76ab4f4c4b314ef00e2094264be2053086b
thewalkers <1518832673@qq.com>
2021-10-10 16:50:20 +0800

    finish: 完成比赛的修改

--- front/src/components/contextAdmin/ContextAdmin.vue
+++ front/src/components/contextAdmin/ContextAdmin.vue
@@ -4,14 +4,74 @@
       <div></div>
       <b-button variant="outline-primary" size="lg"> 添加比赛 </b-button>
     </div>
+    <!-- 修改比赛开始 -->
+    <b-modal
+      id="updateContext"
+      ref="updateContext"
+      title="修改表单"
+      hide-footer
+    >
+      <b-form-group label="比赛名称">
+        <b-form-input
+          v-model="$v.updateParams.title.$model"
+          required
+          :state="validateState1('title')"
+          placeholder="请输入比赛名称"
+        ></b-form-input>
+        <b-form-invalid-feedback :state="validateState1('title')">
+          比赛名称为必填字段
+        </b-form-invalid-feedback>
+      </b-form-group>
+      <b-form-group label="发起人">
+        <b-form-input
+          v-model="$v.updateParams.author.$model"
+          required
+          :state="validateState1('author')"
+          placeholder="请输入比赛的发起人"
+        ></b-form-input>
+        <b-form-invalid-feedback :state="validateState1('author')">
+          比赛的发起人为必填字段
+        </b-form-invalid-feedback>
+      </b-form-group>
+      <b-form-group label="开始时间">
+        <b-form-input
+          v-model="$v.updateParams.startTime.$model"
+          required
+          :state="validateState1('startTime')"
+          placeholder="时间格式：xx-xx-xx xx:xx:xx"
+        ></b-form-input>
+        <b-form-invalid-feedback :state="validateState1('startTime')">
+          比赛的开始时间为必填字段
+        </b-form-invalid-feedback>
+      </b-form-group>
+      <b-form-group label="结束时间">
+        <b-form-input
+          v-model="$v.updateParams.endTime.$model"
+          required
+          :state="validateState1('endTime')"
+          placeholder="时间格式：xx-xx-xx xx:xx:xx"
+        ></b-form-input>
+        <b-form-invalid-feedback :state="validateState1('endTime')">
+          比赛的结束时间为必填字段
+        </b-form-invalid-feedback>
+      </b-form-group>
+      <!-- 按钮 -->
+      <b-form-group>
+        <b-button variant="outline-info" block @click="updateContext"
+          >修改</b-button
+        >
+      </b-form-group>
+    </b-modal>
+    <!-- 修改比赛结束 -->
     <b-card class="mt-3">
       <b-table hover :items="contexts" :fields="fields" class="mt-3">
         <template #cell(context_id)="data">
           <b-button
             variant="primary"
-            @click="updateContext(data.value)"
+            @click="setUpdateID(data.value)"
             size="sm"
             pill
+            v-b-modal.updateContext
           >
             修改
           </b-button>
@@ -48,6 +108,7 @@
 </template>
 
 <script>
+import { required } from 'vuelidate/lib/validators';
 import contextService from '../../service/context';
 
 export default {
@@ -73,7 +134,33 @@ export default {
       deleteParams: {
         contextID: 1,
       },
+      // 更新表单相关
+      updateParams: {
+        contextID: 1,
+        title: '',
+        startTime: '',
+        endTime: '',
+        author: '',
+      },
     };
+  },
+
+  // 数据校验部分
+  validations: {
+    updateParams: {
+      title: {
+        required,
+      },
+      startTime: {
+        required,
+      },
+      endTime: {
+        required,
+      },
+      author: {
+        required,
+      },
+    },
   },
 
   methods: {
@@ -101,8 +188,50 @@ export default {
       });
     },
 
-    updateContext(id) {
-      console.log(id);
+    setUpdateID(id) {
+      sessionStorage.setItem('context_id', id);
+    },
+
+    clearUpdateParams() {
+      this.updateParams.title = '';
+      this.updateParams.startTime = '';
+      this.updateParams.endTime = '';
+      this.updateParams.author = '';
+      // eslint-disable-next-line dot-notation
+      this.$refs['updateContext'].hide();
+      this.$v.$reset();
+    },
+
+    async updateContext() {
+      this.$v.updateParams.$touch();
+      if (this.$v.updateParams.$anyError) {
+        return;
+      }
+
+      this.updateParams.contextID = parseInt(sessionStorage.getItem('context_id'), 10);
+
+      await contextService.updateContext(this.updateParams).then(() => {
+        this.$bvToast.toast('更新比赛成功', {
+          title: '更新成功',
+          variant: 'success',
+          solid: true,
+        });
+
+        this.clearUpdateParams();
+        this.getContextList();
+      }).catch((err) => {
+        this.$bvToast.toast(err.response.data.msg, {
+          title: '更新失败',
+          variant: 'danger',
+          solid: true,
+        });
+      });
+    },
+
+    validateState1(name) {
+      // 这里是 es6 的解构赋值
+      const { $dirty, $error } = this.$v.updateParams[name];
+      return $dirty ? !$error : null;
     },
   },
 
--- front/src/service/context.js
+++ front/src/service/context.js
@@ -9,8 +9,13 @@ const getContext = (id) => request.get(`context/${id}`);
 // 删除比赛
 const deleteContext = ({ contextID }) => request.delete('context', { params: { contextID } });
 
+// 更新比赛
+// eslint-disable-next-line object-curly-newline
+const updateContext = ({ contextID, title, startTime, endTime, author }) => request.put('context', { contextID, title, startTime, endTime, author });
+
 export default {
   getContextList,
   getContext,
   deleteContext,
+  updateContext,
 };
