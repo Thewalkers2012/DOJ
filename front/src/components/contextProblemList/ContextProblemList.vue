c8d7319c3cf2b85b07ae1648ea8fb16c764be5b3
thewalkers <1518832673@qq.com>
2021-10-17 14:48:37 +0800

    finish：完成前端的显示

--- front/src/components/contextProblemList/ContextProblemList.vue
+++ front/src/components/contextProblemList/ContextProblemList.vue
@@ -1,6 +1,24 @@
 <template>
   <div>
-    <h1>fuck</h1>
+    <b-list-group class="mt-3">
+      <b-list-group-item
+        class="d-flex justify-content-between align-items-center"
+      >
+        <strong>题目</strong>
+        <strong>通过率</strong>
+      </b-list-group-item>
+      <b-list-group-item
+        button
+        v-for="item in problems"
+        v-bind:key="item.problem_id"
+        class="d-flex justify-content-between align-items-center"
+        variant="default"
+        @click="ToProblem(item.problem_id)"
+      >
+        <div>{{ item.problem_name }}</div>
+        <div>100/500</div>
+      </b-list-group-item>
+    </b-list-group>
   </div>
 </template>
 
@@ -23,6 +41,9 @@ export default {
 
       const { data: res } = await contextService.getAllContextProblem(this.problemListParams);
       this.problems = res.data.problems;
+    },
+    ToProblem(id) {
+      console.log(id);
     },
   },
 
